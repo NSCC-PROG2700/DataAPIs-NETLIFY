3f94cb25134539765db4db137283b46c0b250ab3
w0244079 <michael.caines@nscc.ca>
2023-03-04 00:19:19 -0400

    test

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -3,7 +3,7 @@ import headers from "../../headers";
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    headers,
+    headers: headers,
     body: JSON.stringify({ message: "Hello World" }),
   };
 }
